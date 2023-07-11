3e35738bc3d24f4c56160ef8db3bc274da4e4ed3
tundeakins <babatundeakinsanmi@gmail.com>
2023-07-11 09:01:35 -0500

    document the function

--- reader.py
+++ reader.py
@@ -4,6 +4,20 @@ import pandas as pd
 
 
 def read_google_sheet(sheet_id = None):
+    """
+    Function to read in a google sheet as csv file using pandas. 
+    Ensure that the sharing setting for the sheet allows anyone with the link to access it.
+
+    Parameters
+    ----------
+    sheet_id : str, optional
+        id of the google sheet extracted from the webpage as */d/sheet_id/edit/*, by default None
+
+    Returns
+    -------
+    DF : pandas dataframe
+        Pandas dataframe containing information on google sheet
+    """
     prefix = "https://docs.google.com/spreadsheets/d/"
     if sheet_id is None: sheet_id = "10aSO3AtjG69PFmjaLTmPm_vEPAAWiZsjmswgIDa6bGY"
     
