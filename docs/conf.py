c470adbc9918489405b09e5de7bad1edcc7783e1
sheydenreich <svenheydenreich@googlemail.com>
2023-07-13 14:43:24 -0500

    adjusted conf

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Tunde, Sven & Anne'
 root_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
