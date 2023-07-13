bb7534e1a13410bd4547f33ccbcd5627b5080b7d
sheydenreich <svenheydenreich@googlemail.com>
2023-07-13 17:03:53 -0500

    bug fixes

--- SpaceShare/run.py
+++ SpaceShare/run.py
@@ -1,14 +1,14 @@
 
-from reader import read_google_sheet
-from optimize_rideshares import optimize
-from write_email import send_emails
+from .reader import read_google_sheet
+from .optimize_rideshares import optimize
+from .write_email import send_emails
 import pandas as pd
 import configparser
 
 # Read the config file
 config = configparser.ConfigParser()
 
-def run(config_file = "default.cfg", dry_run = False):
+def run_spaceshare(config_file = "default.cfg", dry_run = False):
     """
     Main function that uses the configuration file to read a google sheet, optimize ride share groups,
     and then send emails to the participants.
@@ -27,7 +27,7 @@ def run(config_file = "default.cfg", dry_run = False):
     email_smtp_domain = config["EMAIL"]["smtp_domain"].replace(" ","")
     email_smtp_port = int(config["EMAIL"]["smtp_port"])
 
-    google_sheet_id = config["GOOGLE_SHEET"]["sheet_id"].replace(" ","")
+    google_sheet_id = config["GOOGLE.SHEET"]["sheet_id"].replace(" ","")
 
     max_waittime = float(config["OPTIMIZATION"]["max_wait_time"])
     max_people_per_car = int(config["OPTIMIZATION"]["max_people_per_car"])
@@ -36,7 +36,7 @@ def run(config_file = "default.cfg", dry_run = False):
 
     df = read_google_sheet(sheet_id=google_sheet_id)
     for kind in ["arrival","departure"]:
-        df = optimize(df,kind=kind,max_waittime=max_waittime,max_people_per_car=max_people_per_car)
+        df = optimize(df,kind=kind,max_time_difference=max_waittime,max_people_per_car=max_people_per_car)
     df.sort_values(by=["arrival_group","departure_group"],inplace=True)
     df.to_csv("optimized_clustering.csv")
     user_input = "n"
